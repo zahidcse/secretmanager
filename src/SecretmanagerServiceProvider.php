e2279e0e62f36b0ef62504edb1800f05f941be6e
Zahid Hasan <zahiislink@gmail.com>
2019-10-12 15:49:48 +0600

    Update and rename UsstreetaddressServiceProvider.php to SecretmanagerServiceProvider.php

---
+++ src/SecretmanagerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zahidcse\Secretmanager;
+
+use Illuminate\Support\ServiceProvider;
+
+class SecretmanagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
